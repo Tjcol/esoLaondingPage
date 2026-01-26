a71d3abd90cc9daf81ace832614a39ed72e81e82
Tjcol <41172230+Tjcol@users.noreply.github.com>
2026-01-26 04:11:02 +0100

    Update constants.ts - for it to link to Esohe line

--- esoLandingPage/src/lib/constants.ts
+++ esoLandingPage/src/lib/constants.ts
@@ -1,1 +1,1 @@
-export const WHATSAPP_LINK = "https://chat.whatsapp.com/CN4XPYVDCYp1Q9sXA5l1nt";
+export const WHATSAPP_LINK = "https://wa.me/2348112843168?text=Hi%21%20Coach%20Esohe.%20I%20would%20like%20to%20register%20for%20the%20GROW%20with%20Coach%20Esohe%20Program";
